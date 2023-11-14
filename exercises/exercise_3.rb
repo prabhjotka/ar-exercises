9d0f25d5396c53f440cf963159a2422e56d9ec55
Prabh <sonukaur68@gmail.com>
2023-11-13 20:25:16 -0700

    excercise4 done

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,11 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+store3=Store.find_by(id:3)
+@store1=store3
+
+
+store = Store.find_by(name: 'Calgary')
+store.destroy
+
+puts "Number of stores in the database: #{Store.count}"
