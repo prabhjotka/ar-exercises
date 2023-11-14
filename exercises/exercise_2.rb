119cf69df39345c55106f44c0bd788247bf7c5d0
Prabh <sonukaur68@gmail.com>
2023-11-13 21:25:42 -0700

    excercise6 done

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -12,6 +12,7 @@ store1=Store.find_by(id:1)
 @store1=store1
 store2=Store.find_by(id:2)
 @store2=store2
-
+store2=Store.find_by(id:3)
+@store3=store2
 store = Store.find_by(name: 'Burnaby')
 store.update(name: 'Calgary')
