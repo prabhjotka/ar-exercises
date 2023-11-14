0549407f4433ddff77b992953cc630dee6c40fa9
Prabh <sonukaur68@gmail.com>
2023-11-13 20:22:16 -0700

    exercise2 done

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,14 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+
+# user = User.find_by(name: 'David')
+# user.update(name: 'Dave')
+
+store1=Store.find_by(id:1)
+@store1=store1
+store2=Store.find_by(id:2)
+@store2=store2
+
+store = Store.find_by(name: 'Burnaby')
+store.update(name: 'Calgary')
